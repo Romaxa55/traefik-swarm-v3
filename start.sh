b1c3b5c23749dac03a493928e3c0212361482a90
Roman SHamagin <roman.shamagin@7vpn.com>
2024-06-19 09:50:08 +0300

    init minio traefik

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export $(cat .env | xargs) && docker stack deploy -c portainer.yml portainer
-docker stack deploy -c docker-compose.yml traefik
+export $(cat .env | xargs) && docker stack deploy -c docker-compose.yml traefik
